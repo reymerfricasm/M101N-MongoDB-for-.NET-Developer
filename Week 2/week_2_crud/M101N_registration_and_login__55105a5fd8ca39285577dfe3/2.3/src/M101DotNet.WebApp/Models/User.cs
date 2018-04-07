1c9fcbab4336ec05d298ac7eb9856f6c8fab858a
unknown <reymerfmoscoso@gmail.com>
2018-04-07 13:48:06 -0400

    First commit, with all the changes.

---
+++ Week 2/week_2_crud/M101N_registration_and_login__55105a5fd8ca39285577dfe3/2.3/src/M101DotNet.WebApp/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace M101DotNet.WebApp.Models
+{
+    public class User
+    {
+        // XXX WORK HERE
+        // create an object suitable for insertion into the user collection
+        // The homework instructions will tell you the schema that the documents 
+        // must follow. Make sure to include Name and Email properties.
+
+        public ObjectId Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+    }
+}
